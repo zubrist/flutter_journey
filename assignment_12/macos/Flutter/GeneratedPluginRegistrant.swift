d98ef05c0225f7a2162a7b690e2080c7306c402c
zubrist <zubrist7@gmail.com>
2026-03-09 05:03:15 +0530

    code updated

--- assignment_12/macos/Flutter/GeneratedPluginRegistrant.swift
+++ assignment_12/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import file_picker
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
 }
